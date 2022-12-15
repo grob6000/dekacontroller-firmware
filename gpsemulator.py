e469f6434a8a25afe3bf89dd427587aeee160c56
George <grob6000@users.noreply.github.com>
2022-12-15 13:56:02 +1100

    Add guff to source files

--- gpsemulator.py
+++ gpsemulator.py
@@ -2,6 +2,22 @@
 
 """gpsemulator.py: Pretends to be a GPS receiver, NEMA protocol. Provide port name as first argument or hardcode below."""
 
+#    gpsemulator.py
+#    Copyright (C) 2022 grob6000 (https://github.com/grob6000)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from cProfile import run
 from operator import xor
 from re import ASCII
@@ -13,6 +29,9 @@ import datetime
 portname = "COM99"
 
 if __name__ == "__main__":
+
+  print ("gpsemulator.py\nCopyright (C) 2022 grob6000 (https://github.com/grob6000)\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under certain conditions.\nFor details see LICENSE at https://github.com/grob6000/dekacontroller-firmware.")
+
   if len(sys.argv) > 1:
     portname = sys.argv[1]
   ser = None
@@ -26,7 +45,7 @@ if __name__ == "__main__":
     print(ser)
     ser.open()
   except:
-    print("Serial port problem, aborting")
+    print("Serial port problem, aborting. If you want to specify port in commandline, pass as first argument (e.g. python gpsemulator.py COM3)")
     quit()
 
   print("Running GPS Emulator on port " + portname)
